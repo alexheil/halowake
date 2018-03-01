44e4d94d60d8ac3cc85e50b12d482750b397921c
alexheil <aheil4250@gmail.com>
2018-03-01 10:17:30 -0500

    memberships

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,7 +14,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.create_location(location_params)
     @user.create_theme(theme_params)
     @user.create_membership(membership_params)
-    # create Stripe customer then edit this in settings
+    # customer = Stripe::Customer.create({
+    #  email: @user.email,
+    # })
   end
 
   # GET /resource/edit
